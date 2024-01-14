7514ad10b71cf91e6877da86e8f09c3cf7ee6f23
kirkum3 <pulenko.evheniy@lll.kpi.ua>
2024-01-14 18:38:50 +0200

    renamed function

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,7 +3,7 @@
 
 let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+const parseDay = (s) => {
   let i;
   for (i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
@@ -12,4 +12,4 @@ _parse_day_ = (s) => {
   }
   return -1;
 };
-module.exports = _parse_day_;
+module.exports = parseDay;
