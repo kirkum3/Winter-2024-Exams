0d61c87c763ecd342bf0a7f41f74a843151499e9
kirkum3 <pulenko.evheniy@lll.kpi.ua>
2024-01-14 23:59:33 +0200

    introduced a const named result, remove the unnecessary else block and added proper indentation for better readability

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,10 @@
 // Return an array without duplicates
 'use strict';
-duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
+const duplicate = (value, N) => {
+  if (N <= 0) {
+    return [];
   }
-};
 
+  return result;
+}
 module.exports = duplicate;
