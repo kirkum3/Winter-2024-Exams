73dfd05ae2aa6e9b58ea187e3a8d86eb8cf3d48f
kirkum3 <pulenko.evheniy@lll.kpi.ua>
2024-01-14 19:02:13 +0200

    renamed functions and added const

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -1,8 +1,8 @@
 // Return an array without duplicates
 
-DISTINCT = (data) => {
-  A = new Set();
-  w = 0;
+const distinct = (data) => {
+  const A = new Set();
+  const w = 0;
   data.forEach((a) => {
     if (A.has(a)) {
       delete data[w];
@@ -15,4 +15,4 @@ DISTINCT = (data) => {
   (x => typeof x === 'number');
 };
 
-module.exports = DISTINCT;
+module.exports = distinct;
