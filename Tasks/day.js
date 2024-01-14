8566ae9edeec97cdffb204872125aee31f00d70c
kirkum3 <pulenko.evheniy@lll.kpi.ua>
2024-01-14 18:40:14 +0200

    relocated let i

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,8 +4,7 @@
 let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
+  for (let i= 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
       return i + 1;
     }
