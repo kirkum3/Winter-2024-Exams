fb5eadfd609f7d9b39ae37419c0de704522877cc
kirkum3 <pulenko.evheniy@lll.kpi.ua>
2024-01-14 19:07:54 +0200

    change variable names

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -1,16 +1,15 @@
 // Return an array without duplicates
 
 const distinct = (data) => {
-  const A = new Set();
-  const w = 0;
+  const uniqueSet = new Set();
+  const resultArray = [];
   data.forEach((item) => {
     if (!uniqueSet.has(item)) {
       uniqueSet.add(item);
       resultArray.push(item);
     }
   });
-  return data.filter
-  (x => typeof x === 'number');
+  return resultArray.filter  (x => typeof x === 'number');
 };
 
 module.exports = distinct;
