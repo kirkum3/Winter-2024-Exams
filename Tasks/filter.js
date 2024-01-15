b9221de588f9d8843a0b7ae77c60633a17e0362d
kirkum3 <pulenko.evheniy@lll.kpi.ua>
2024-01-15 11:44:38 +0200

    rename: remove =>  indicesToRemove, C => element, x => index

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,14 +1,14 @@
 // Filter array by type name
 'use strict';
 const filterByType = (arr, type) => {
-  remove = [];
-  for (C of arr) {
-    x = arr.indexOf(C);
-    if (typeof arr[x] !== type) {
+  const indicesToRemove = [];
+  for (element of arr) {
+    index = arr.indexOf(element);
+    if (typeof arr[index] !== type) {
       remove.unshift(x);
     }
   }
-  for (x of remove) arr.splice(x, 1);
+  for (index of indicesToRemove) arr.splice(index, 1);
   return arr;
 };
 
